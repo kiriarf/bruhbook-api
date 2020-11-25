a5a2ded469155525069b3647ebb4d2d1d3d331ef
jotkafomat <kjxl.wp.pl>
2020-11-25 15:02:36 +0000

    add session Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:create]
   root to: "static#home"
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
