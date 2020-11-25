d8cb7f40ac17d46674c897f154b9a7b87d0b54ad
jotkafomat <kjxl.wp.pl>
2020-11-25 20:39:35 +0000

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts only: [:index, :show, :create, :update, :destroy]
   resources :registrations, only: [:create]
   resources :sessions, only: [:create]
   resources :users, only: [:index, :show]
   delete :logout, to: "sessions#logout"
-  get :logged_in, to: "sessions#logged_in" 
+  get :logged_in, to: "sessions#logged_in"
   root to: "static#home"
 end
