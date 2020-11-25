66435219fbe2385a857b32c6c0a88cddd5cc0043
Kiril <kiril@olgas-air.mynet>
2020-11-25 15:47:55 +0000

    added user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "kiril", email:"k@d.com", password: "1234")
+User.create(name: "kris", email:"k@j.com", password: "1234")
+User.create(name: "maggie", email:"m@h.com", password: "1234")
+User.create(name: "mace", email:"m@a.com", password: "1234")
+
+# Post.create(text_content: "hey, this is kiril", user_id: 1)
+# Post.create(text_content: "hey, this is kris", user_id: 2)
+# Post.create(text_content: "hey, this is maggie", user_id: 3)
+# Post.create(text_content: "hey, this is mace", user_id: 4)
