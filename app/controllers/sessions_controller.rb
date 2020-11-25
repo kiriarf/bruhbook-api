80a2141a0ec87114388083df74c208c36ce7c341
Kiril <kiril@olgas-air.mynet>
2020-11-25 16:23:57 +0000

    add showcurrent route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,12 @@ class SessionsController < ApplicationController
     end
   end
 
+  def showcurrent
+    render json: {
+      user: @current_user
+    }
+  end
+
   def logged_in
     if @current_user
       render json: {
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :sessions, only: [:create]
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in" 
+  get :showcurrent, to: "sessions#showcurrent"
   root to: "static#home"
 end
